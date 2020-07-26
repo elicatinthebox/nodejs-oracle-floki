4c7f3f08fc6f4eb8f7f47eee0be097b6d577a1ab
Lorenzo Zaccagnini <lorenzozcg@gmail.com>
2020-07-26 15:19:43 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+const PORT = process.env.PORT || 8000
+console.log(`PORT ${PORT}`)
 
+fastify.get('/', async (request, reply) => {
+    return { hello: 'world' }
+  })
 const fastify = require('fastify')({ logger: true })
 
 fastify.get('/', async (request, reply) => {
@@ -9,7 +14,7 @@ fastify.get('/', async (request, reply) => {
 fastify.register(require('./routes/v3/utils/route_utils'), { prefix: '/v3/utils' })
 fastify.register(require('./routes/v3/routes'), { prefix: '/v3/contract' })
 
-fastify.listen(process.env.PORT || 8000, err => {
+fastify.listen(PORT, err => {
   if (err) throw err
   console.log(`server listening on ${fastify.server.address().port}`)
 })
