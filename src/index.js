fe56406cd28d47dfd0b621033483a3131902beae
LorenzoZaccagnini <lorenzozcg@gmail.com>
2020-07-27 16:43:55 +0200

    new contract

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
-const fastify = require('fastify')({ logger: true })
+const fastify = require('fastify')({
+  logger: true
+})
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 const PORT = process.env.PORT || 8000
 console.log(`PORT ${PORT}`)
 
-fastify.register(require('fastify-cors'), { 
-    origin: false
+fastify.register(require('fastify-cors'), {
+  origin: ["http://localhost:3000", /\.devoleum\.com$/]
 })
 
-
 fastify.get('/', async (request, reply) => {
-    return { hello: 'world' }
-  })
+  return {
+    hello: 'world'
+  }
+})
 
-fastify.register(require('./routes/v3/utils/route_utils'), { prefix: '/v3/utils' })
-fastify.register(require('./routes/v3/routes'), { prefix: '/v3/contract' })
+fastify.register(require('./routes/v3/utils/route_utils'), {
+  prefix: '/v3/utils'
+})
+fastify.register(require('./routes/v3/routes'), {
+  prefix: '/v3/contract'
+})
 
 fastify.listen(PORT, '0.0.0.0', err => {
   if (err) throw err
